e846fc3164b1ca60f2a340109e66edf7b4fc3ffb
sjpaikzionex <sj_paik@zionex.com>
2021-08-18 11:04:38 +0900

    slider and input criteria update

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,105 @@
 import { Component } from 'react';
 import HeatMap from './heatmap';
+import { Range, getTrackBackground } from 'react-range';
+import axios from 'axios';
 import Threshold from './threshold';
+const d3 = require('d3');
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.rangehandler = this.rangehandler.bind(this)
+    }
+
+    target_count = 1500;
+    maxCov = [1.5]
+
+    rangehandler(v) {
+        this.maxCov = v
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        d3.selectAll('#selected').remove()
+
+        let dat = Array.from(d3.selectAll('#heatbox').data());
+        dat.sort((a, b) => {
+            if (a.qty_pct < b.qty_pct)
+                return 1;
+
+            if (a.qty_pct > b.qty_pct)
+                return -1;
+
+            if (a.cov_lbl > b.cov_lbl)
+                return 1;
+
+            if (a.cov_lbl < b.cov_lbl)
+                return -1;
+
+            return 0;
+        });
+
+        const maxCov = this.maxCov[0]
+        const selected = [];
+        let select_count = 0;
+        for (let i = 0; i < dat.length; i++) {
+            if (parseFloat(dat[i].cov_lbl) <= maxCov && dat[i].sub_count !== 0) {
+                selected.push(dat[i]);
+                select_count += dat[i].sub_count;
+            }
+            if (select_count >= this.target_count) {
+                break;
+            }
+        }
+
+        const boxwidth = d3.select('#heatbox').attr('width');
+        const boxheight = d3.select('#heatbox').attr('height');
+
+        const selectBox = d3.selectAll('#heatbox').filter(d => {return selected.includes(d);})
+
+        let selectXY = []
+        for (let i = 0; i < selectBox._groups[0].length; i++) {
+            selectXY.push({
+                x: d3.select(selectBox._groups[0][i]).attr('x'),
+                y: d3.select(selectBox._groups[0][i]).attr('y')
+            })
+        }
+
+        const map = d3.select('#heatmap')
+
+        for (let i = 0; i < selectXY.length; i++) {
+            map
+                .append('rect')
+                .attr('id', 'selected')
+                .attr('x', selectXY[i].x)
+                .attr('y', selectXY[i].y)
+                .attr('width', boxwidth)
+                .attr('height', boxheight)
+                .attr('fill', d3.rgb(0, 0, 10, 0.2))
+                .style('stroke-width', '0.5');
+        }
+
+        axios
+            .post('http://localhost:5000/drag', selected)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     render () {
         return (
             <div>
                 <h3>Heat Map</h3>
                 <HeatMap size={[820, 500]}/>
                 <h3>Select Criteria</h3>
-                <Threshold target_count={1500}/>
+                <form onSubmit={this.handleSubmit}>
+                    <Threshold handler={this.rangehandler}/>
+                    <button type='submit'>Search</button>
+                </form>
             </div>
         );
     }
--- front/src/threshold.js
+++ front/src/threshold.js
@@ -1,215 +1,104 @@
 import React, { Component } from 'react';
 import { Range, getTrackBackground } from 'react-range';
 import './threshold.css';
-import axios from 'axios';
-const d3 = require('d3');
 
 class Threshold extends Component {
     state = {
-        cpu: '',
-        time: '',
-        target_count: 1000,
         maxCov: [1.5]
     }
 
     constructor(props) {
         super(props);
-        this.target_count = props.target_count;
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handler = this.handler.bind(this)
     }
 
-    handleChange = (e) => {
+    handler(v) {
         this.setState({
-            [e.target.name]: e.target.value
+            maxCov: v
         })
-
-        if (e.target.name === 'cpu') {
-            //20 cpu, 1h = 1000 items
-            if (e.target.value === '') {
-                this.setState({
-                    'time': ''
-                });
-
-            } else {
-                this.setState({
-                    'time': 20 / e.target.value * this.state.target_count / 1000
-                });
-            }
-        }
-        if (e.target.name === 'time') {
-            let new_count = this.state.cpu / 20 * e.target.value / 1 * 1000;
-
-            if (this.state.cpu === '' || e.target.value === '') {
-                new_count = 1000;
-            }
-
-            this.setState({
-                'target_count': parseInt(new_count)
-            });
-        }
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        d3.selectAll('#selected').remove()
-        const target_count = this.target_count;
-
-        let dat = Array.from(d3.selectAll('#heatbox').data());
-        dat.sort((a, b) => {
-            if (a.qty_pct < b.qty_pct)
-                return 1;
-
-            if (a.qty_pct > b.qty_pct)
-                return -1;
-
-            if (a.cov_lbl > b.cov_lbl)
-                return 1;
-
-            if (a.cov_lbl < b.cov_lbl)
-                return -1;
-
-            return 0;
-        });
-
-        const maxCov = this.state.maxCov[0]
-        const selected = [];
-        let select_count = 0;
-        for (let i = 0; i < dat.length; i++) {
-            if (parseFloat(dat[i].cov_lbl) <= maxCov && dat[i].sub_count !== 0) {
-                selected.push(dat[i]);
-                select_count += dat[i].sub_count;
-            }
-            if (select_count >= target_count) {
-                break;
-            }
-        }
-
-        const boxwidth = d3.select('#heatbox').attr('width');
-        const boxheight = d3.select('#heatbox').attr('height');
-
-        const selectBox = d3.selectAll('#heatbox').filter(d => {return selected.includes(d);})
-
-        let selectXY = []
-        for (let i = 0; i < selectBox._groups[0].length; i++) {
-            selectXY.push({
-                x: d3.select(selectBox._groups[0][i]).attr('x'),
-                y: d3.select(selectBox._groups[0][i]).attr('y')
-            })
-        }
-
-        const map = d3.select('#heatmap')
-
-        for (let i = 0; i < selectXY.length; i++) {
-            map
-                .append('rect')
-                .attr('id', 'selected')
-                .attr('x', selectXY[i].x)
-                .attr('y', selectXY[i].y)
-                .attr('width', boxwidth)
-                .attr('height', boxheight)
-                .attr('fill', d3.rgb(0, 0, 10, 0.2))
-                .style('stroke-width', '0.5');
-        }
-
-        axios
-            .post('http://localhost:5000/drag', selected)
-            .then(response => {
-                console.log(response);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+        this.props.handler(v)
     }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <div className='threshold-container'>
-                        <label>Target Count: {this.target_count}</label>
-                        <label>max cov</label>
-                        <Range
-                            label={true}
-                            name='maxCov'
-                            step={0.1}
-                            min={0}
-                            max={1.9}
-                            values={this.state.maxCov}
-                            onChange={(v) => this.setState({'maxCov': v})}
-                            renderTrack={ ({ props, children }) => (
-                                <div
-                                    onMouseDown={props.onMouseDown}
-                                    onTouchStart={props.onTouchStart}
-                                    style={{
-                                        ...props.style,
-                                        height: "36px",
-                                        display: "flex",
-                                        width: "100%"
-                                    }}
-                                >
-                                    <div
-                                        ref={props.ref}
-                                        style={{
-                                        height: "5px",
-                                        width: "100%",
-                                        borderRadius: "4px",
-                                        background: getTrackBackground({
-                                            values: this.state.maxCov,
-                                            colors: ["#548BF4", "#ccc"],
-                                            min: 0,
-                                            max: 1.9
-                                        }),
-                                        alignSelf: "center"
-                                        }}
-                                    >
-                                        {children}
-                                    </div>
-                                </div>
-                            )}
-                            renderThumb={ ({props, isDragged}) => (
-                                <div
-                                    {...props}
-                                    style={{
-                                        ...props.style,
-                                        height: "20px",
-                                        width: "20px",
-                                        borderRadius: "4px",
-                                        backgroundColor: "#FFF",
-                                        display: "flex",
-                                        justifyContent: "center",
-                                        alignItems: "center",
-                                        boxShadow: "0px 2px 6px #AAA"
-                                    }}
-                                >
-                                    <div
-                                        style={{
-                                            height: "8px",
-                                            width: "3px",
-                                            backgroundColor: isDragged ? "#548BF4" : "#CCC"
-                                        }}
-                                    />
-                                    <div
-                                        style={{
-                                            position: 'absolute',
-                                            top: '-28px',
-                                            color: '#fff',
-                                            fontSize: '12px',
-                                            fontFamily: 'Calibri',
-                                            padding: '3px',
-                                            borderRadius: '4px',
-                                            backgroundColor: '#548BF4'
-                                        }}
-                                    >
-                                        {this.state.maxCov}
-                                    </div>
-                                </div>
-                            )}
-                        />
-                    </div>
-                    <div className='form-submit'>
-                        <input type='submit' value='search' />
-                    </div>
-                </form>
+                <Range
+                    label={true}
+                    name='maxCov'
+                    step={0.1}
+                    min={0}
+                    max={1.9}
+                    values={this.state.maxCov}
+                    onChange={this.handler}
+                    renderTrack={ ({ props, children }) => (
+                        <div
+                            onMouseDown={props.onMouseDown}
+                            onTouchStart={props.onTouchStart}
+                            style={{
+                                ...props.style,
+                                height: "36px",
+                                display: "flex",
+                                width: "100%"
+                            }}
+                        >
+                            <div
+                                ref={props.ref}
+                                style={{
+                                    height: "5px",
+                                    width: "100%",
+                                    borderRadius: "4px",
+                                    background: getTrackBackground({
+                                        values: this.state.maxCov,
+                                        colors: ["#548BF4", "#ccc"],
+                                        min: 0,
+                                        max: 1.9
+                                    }),
+                                    alignSelf: "center"
+                                }}
+                            >
+                                {children}
+                            </div>
+                        </div>
+                    )}
+                    renderThumb={ ({props, isDragged}) => (
+                        <div
+                            {...props}
+                            style={{
+                                ...props.style,
+                                height: "20px",
+                                width: "20px",
+                                borderRadius: "4px",
+                                backgroundColor: "#FFF",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                boxShadow: "0px 2px 6px #AAA"
+                            }}
+                        >
+                            <div
+                                style={{
+                                    height: "8px",
+                                    width: "3px",
+                                    backgroundColor: isDragged ? "#548BF4" : "#CCC"
+                                }}
+                            />
+                            <div
+                                style={{
+                                    position: 'absolute',
+                                    top: '-28px',
+                                    color: '#fff',
+                                    fontSize: '12px',
+                                    fontFamily: 'Calibri',
+                                    padding: '3px',
+                                    borderRadius: '4px',
+                                    backgroundColor: '#548BF4'
+                                }}
+                            >
+                                {this.state.maxCov}
+                            </div>
+                        </div>
+                    )}
+                />
             </div>
         );
     }
